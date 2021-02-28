d15970de53e67ba98d65e67f3a0d6e5c12dbecb0
codingisverygoodforyourhealth <72354340+codingisverygoodforyourhealth@users.noreply.github.com>
2021-02-28 13:15:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var 
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
